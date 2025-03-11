5dc23c02d4e290a7f7784e28c45f0411c96c2d91
Douxx <douxx@douxx.tech>
2025-03-11 14:15:16 +0100

    acao set on potato.the... and not *

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://potato.theserver.life");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
